be2c81f316312b7c3687af02fba492bc09d0ac75
Haitao.Huang <talhuang1231@gmail.com>
2026-05-23 22:27:08 +0800

    chore: remove rockhopper patches, move supabase migrations into saas
    - Remove local patches/rockhopper-v2.0.7 (use upstream dependency instead)
    - Move supabase/ directory from project root into saas/

--- patches/rockhopper-v2.0.7/scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-go test ./...
-dotenv -f .env.local -- go run ./cmd/rockhopper --config rockhopper_mysql_local.yaml status --debug
-dotenv -f .env.local -- go run ./cmd/rockhopper --config rockhopper_mysql_local.yaml down --all
-dotenv -f .env.local -- go run ./cmd/rockhopper --config rockhopper_mysql_local.yaml up
-dotenv -f .env.local -- go run ./cmd/rockhopper --config rockhopper_mysql_local.yaml compile --output pkg/testing/migrations
--- patches/rockhopper-v2.0.7/testdata/migrations/20201211175751_fix_symbol_length.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE trades MODIFY COLUMN symbol VARCHAR(9);
-ALTER TABLE orders MODIFY COLUMN symbol VARCHAR(9);
-
--- +goose Down
-ALTER TABLE trades MODIFY COLUMN symbol VARCHAR(8);
-ALTER TABLE orders MODIFY COLUMN symbol VARCHAR(8);
